4c46e7dde1d4c10a4bd2be134691c7bcd572a9d7
Yarkinov <yarkinov@ya.ru>
2020-11-26 00:46:51 +0500

    Home work, test2

--- src/test/java/MainClassTest.java
+++ src/test/java/MainClassTest.java
@@ -6,6 +6,12 @@ public class MainClassTest {
     @Test
     public void testGetLocalNumber() {
         MainClass mainClass = new MainClass();
-        Assert.assertTrue("Method getLocalNumber returned !14",mainClass.getLocalNumber() == 14);
+        Assert.assertTrue("Method getLocalNumber returned !14", mainClass.getLocalNumber() == 14);
+    }
+
+    @Test
+    public void testGetClassNumber() {
+        MainClass mainClass = new MainClass();
+        Assert.assertTrue("Method testGetClassNumber returned value < 45", mainClass.getClassNumber() > 45);
     }
 }
