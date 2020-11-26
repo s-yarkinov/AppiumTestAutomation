b9e716be972071908ed0b5e71baa26fd70d0c5cf
Yarkinov <yarkinov@ya.ru>
2020-11-26 22:03:12 +0500

    delete all

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,13 +1,2 @@
 public class MainClass {
-
-    private int classNumber = 20;
-
-    public int getClassNumber() {
-        return this.classNumber;
-    }
-
-    public int getLocalNumber()
-    {
-        return 14;
-    }
 }
--- src/test/java/MainClassTest.java
+++ src/test/java/MainClassTest.java
@@ -3,15 +3,4 @@ import org.junit.Test;
 
 public class MainClassTest {
 
-    @Test
-    public void testGetLocalNumber() {
-        MainClass mainClass = new MainClass();
-        Assert.assertTrue("Method getLocalNumber returned !14", mainClass.getLocalNumber() == 14);
-    }
-
-    @Test
-    public void testGetClassNumber() {
-        MainClass mainClass = new MainClass();
-        Assert.assertTrue("Method testGetClassNumber returned value < 45", mainClass.getClassNumber() > 45);
-    }
 }
