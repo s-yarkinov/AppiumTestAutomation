16473b6cb685415197bcbabcd81164838548cee3
Yarkinov <yarkinov@ya.ru>
2020-11-26 21:56:02 +0500

    delete all

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,13 +1,4 @@
 public class MainClass {
 
-    private int classNumber = 20;
 
-    public int getClassNumber() {
-        return this.classNumber;
-    }
-
-    public int getLocalNumber()
-    {
-        return 14;
-    }
 }
